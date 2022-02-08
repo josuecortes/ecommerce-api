95e1ebe78a0a54ee6a718bc6d18d49c280967bd7
Josue Cortes <josue.cort3s@gmail.com>
2022-02-08 10:55:07 -0300

    first commit

---
+++ app/controllers/admin/v1/users_controller.rb
@@ -0,0 +1,44 @@
+module Admin::V1
+  class UsersController < ApiController
+    before_action :load_user, only: [:update, :destroy]
+
+    def index
+      @users = User.all
+    end
+
+    def create
+      @user = User.new
+      @user.attributes = user_params
+      save_user!
+    end
+
+    def update
+      @user.attributes = user_params
+      save_user!
+    end
+
+    def destroy
+      @user.destroy!
+    rescue
+      render_error(fields: @user.errors.messages)
+    end
+
+    private
+
+    def user_params
+      return {} unless params.has_key?(:user)
+      params.require(:user).permit(:id, :name, :email, :profile, :password, :password_confirmation)
+    end
+
+    def load_user
+      @user = User.find(params[:id])
+    end
+
+    def save_user!
+      @user.save!
+      render :show
+    rescue
+      render_error(fields: @user.errors.messages)
+    end
+  end
+end
